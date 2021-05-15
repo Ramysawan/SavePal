d45ea073f498d54d4a36d6199de5528ad44b8481
ramysawan <ramy_Sawan@hotmail.com>
2021-05-15 19:06:45 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { ElectronicsComponent } from './components/electronics/electronics.component';
-import { FashionComponent } from './components/fashion/fashion.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -20,8 +18,6 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent},
   { path: 'contact', component: ContactComponent },
-  { path: 'electronics', component: ElectronicsComponent },
-  { path: 'fashion', component: FashionComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 
 ];
@@ -35,8 +31,6 @@ const routes: Routes = [
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    ElectronicsComponent,
-    FashionComponent,
     FooterComponent,
     NavbarComponent,
   ],
